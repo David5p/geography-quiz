0296f95d22f314b7f24665bb7515a2437c29f60d
David5p <k9glpuwd@students.codeinstitute.net>
2025-06-12 17:42:19 +0200

    Added js event listeners to iniiate game start

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const capitalsButton = document.getElementById("capitals-btn")
+const countriesButton = document.getElementById("countries-btn")
+
+capitalsButton.addEventListener('click', startGame)
+countriesButton.addEventListener('click', startGame)
+
+
+function startGame() {
+    console.log("Let the games begin")
+}
+    
+    function setNextCountryQuestion() {
+
+};
+
+function setNextCapitalQuestion() {
+
+};
+
+function selectAnswer() {
+
+};
